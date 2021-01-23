cf3fd4e4261a190ed0756bcd201cb7be5806a882
olliegardner <olliegardner@gmail.com>
2021-01-23 17:48:44 +0000

    scrapes text from tweets and prints to console

--- content.js
+++ content.js
@@ -1,3 +1,19 @@
 $(function () {
-  document.body.style.backgroundColor = "red";
+  setTimeout(function () {
+    var tweets = document.querySelectorAll("[data-testid='tweet']");
+
+    tweets.forEach((tweet) => {
+      var text = tweet.children[1].children[1].children[0].children[0];
+      var tweetText = "";
+
+      if (text !== undefined) {
+        for (let span of text.children) {
+          if (span.firstChild.tagName != "DIV") {
+            tweetText += span.firstChild.textContent;
+          }
+        }
+      }
+      console.log(tweetText);
+    });
+  }, 3000);
 });
