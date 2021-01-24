a0c0d9a0234063374cdc1b219fcfa4ffe8c6edc8
Fraser Dale <fraserdale2@gmail.com>
2021-01-24 01:04:34 +0000

    added colours to pills

--- content.js
+++ content.js
@@ -34,6 +34,15 @@ $(() => {
             "sexual_explicit",
             "toxicity",
         ];
+        var colours = {
+            identity_attack: "#9C27B0",
+            insult: "#3959AB",
+            threat: "#00BCD4",
+            obscene: "#FF8F00",
+            severe_toxicity: "#4CAF50",
+            sexual_explicit: "#E91E63",
+            toxicity: "#827717",
+        };
 
         toxicity.load(threshold, labelsToInclude).then((model) => {
             // use the `model` object to label sentences.
@@ -47,7 +56,9 @@ $(() => {
                         ).click((e) => revealTweet(e));
 
                         var pill = $(
-                            `<span class="badge rounded-pill bg-primary pill">${label.label}: ${parseInt(label.results[x].probabilities[1]*100)}%</span>`
+                            `<span class="badge rounded-pill pill" style='background-color:${colours[label.label]}'">${
+                label.label.replace('_',' ')
+              }: ${parseInt(label.results[x].probabilities[1] * 100)}%</span>`
                         );
 
                         var parent = tweets[x].parentElement;
@@ -58,7 +69,7 @@ $(() => {
                                 addBlur(parent, revealButton);
                                 pillDiv.appendTo(parent.parentElement);
                             } else {
-                                pill.appendTo(tweets[x].parentElement.nextSibling.nextSibling)
+                                pill.appendTo(tweets[x].parentElement.nextSibling.nextSibling);
                             }
                         }
                     }
@@ -70,12 +81,11 @@ $(() => {
     revealTweet = (e) => {
         e.target.previousElementSibling.classList.remove("blur");
         e.target.classList.add("hidden");
-        e.target.nextSibling.classList.add("hidden")
+        e.target.nextSibling.classList.add("hidden");
     };
 
     addBlur = (parent, revealButton) => {
         parent.classList.add("blur");
         revealButton.appendTo(parent.parentElement);
     };
-
 });
