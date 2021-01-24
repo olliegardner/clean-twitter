d186057b423739e5aaf0ff1850c07bd32de50031
olliegardner <olliegardner@gmail.com>
2021-01-24 15:19:54 +0000

    added twitter profiling classification

--- background.js
+++ background.js
@@ -1,30 +1,36 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.predictions != null) {
-        chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            let username = tabs[0].url.split("/")[3];
-            var req = new XMLHttpRequest();
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.predictions != null) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let username = tabs[0].url.split("/")[3];
+      var req = new XMLHttpRequest();
 
+      req.open("POST", request.config.googleCloudURL);
+      req.setRequestHeader("Content-Type", "application/json");
 
-            req.open(
-                "POST",
-                request.config.googleCloudURL,
+      let body = JSON.stringify({
+        username: username,
+        content: request.predictions,
+      });
 
-            );
-            req.setRequestHeader(
-                "Content-Type", "application/json"
-            );
+      req.send(body);
 
-            let body = JSON.stringify({
-                username: username,
-                content: request.predictions,
-            })
+      sendResponse({ username: username });
+    });
+  } else if (request.predictions == null && request.config != null) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      let username = tabs[0].url.split("/")[3];
+      var req = new XMLHttpRequest();
 
-            req.send(
-                body
-            )
+      var url = request.config.googleCloudURL + "?username=" + username;
 
-            sendResponse({ "username": username });
-        });
-    }
-    return true;
+      req.open("GET", url, true);
+      req.setRequestHeader("Content-Type", "application/json");
+      req.send();
+
+      req.onload = () => {
+        sendResponse(req.response);
+      };
+    });
+  }
+  return true;
 });
--- content.js
+++ content.js
@@ -1,141 +1,176 @@
 var loadedTweets = [];
+var count = 0;
 
-$(() => {
-    setTimeout(async() => {
-        await getTweets();
-    }, 2000);
-});
-
-setInterval(async() => {
-    await getTweets();
-}, 2000);
-
-loadModel = async() => {
-    return await toxicity.load(0.75, [
-        "identity_attack",
-        "insult",
-        "threat",
-        "obscene",
-        "severe_toxicity",
-        "sexual_explicit",
-        "toxicity",
-    ]);
+var colours = {
+  identity_attack: "#9C27B0",
+  insult: "#3959AB",
+  threat: "#00BCD4",
+  obscene: "#FF8F00",
+  severe_toxicity: "#4CAF50",
+  sexual_explicit: "#E91E63",
+  toxicity: "#827717",
 };
 
-getTweets = async() => {
-    var tweets = document.querySelectorAll("[data-testid='tweet']");
+$(() => {
+  setTimeout(async () => {
+    await getTweets();
+    profiling();
+  }, 2000);
+});
 
-    tweets.forEach((tweet) => {
-        var tweetText = getTweetText(tweet);
+setInterval(async () => {
+  await getTweets();
+}, 2000);
 
-        if (!loadedTweets.find((t) => t.content == tweetText)) {
-            loadedTweets.push({ content: tweetText, pills: [], pillLabels: [] });
-        }
-    });
+loadModel = async () => {
+  return await toxicity.load(0.75, [
+    "identity_attack",
+    "insult",
+    "threat",
+    "obscene",
+    "severe_toxicity",
+    "sexual_explicit",
+    "toxicity",
+  ]);
+};
 
-    var colours = {
-        identity_attack: "#9C27B0",
-        insult: "#3959AB",
-        threat: "#00BCD4",
-        obscene: "#FF8F00",
-        severe_toxicity: "#4CAF50",
-        sexual_explicit: "#E91E63",
-        toxicity: "#827717",
-    };
+getTweets = async () => {
+  var tweets = document.querySelectorAll("[data-testid='tweet']");
 
-    var model = await loadModel();
-    var text = loadedTweets.map((tweet) => tweet.content);
+  tweets.forEach((tweet) => {
+    var tweetText = getTweetText(tweet);
 
-    // use the `model` object to label sentences.
-    model.classify(text).then((predictions) => {
-        predictions.forEach((label) => {
-            for (x in label.results) {
-                var percentage = parseInt(label.results[x].probabilities[1] * 100);
+    if (!loadedTweets.find((t) => t.content == tweetText)) {
+      loadedTweets.push({ content: tweetText, pills: [], pillLabels: [] });
+    }
+  });
 
-                var pill = $(
-                    `<span class="badge rounded-pill pill" style='background-color:${
+  var model = await loadModel();
+  var text = loadedTweets.map((tweet) => tweet.content);
+
+  // use the `model` object to label sentences.
+  model.classify(text).then((predictions) => {
+    predictions.forEach((label) => {
+      for (x in label.results) {
+        var percentage = parseInt(label.results[x].probabilities[1] * 100);
+
+        var pill = $(
+          `<span class="badge rounded-pill pill" style='background-color:${
             colours[label.label]
           }'">${label.label.replace("_", " ")}: ${percentage}%</span>`
-                );
+        );
 
-                if (label.results[x].match || label.results[x].match == null) {
-                    // if not in loaded tweets array
-                    var t = loadedTweets.find((t) => t.content == text[x]);
+        if (label.results[x].match || label.results[x].match == null) {
+          // if not in loaded tweets array
+          var t = loadedTweets.find((t) => t.content == text[x]);
 
-                    if (!t.pillLabels.includes(label.label)) {
-                        loadedTweets
-                            .find((tweet) => tweet.content == text[x])
-                            .pills.push(pill);
-                        loadedTweets
-                            .find((tweet) => tweet.content == text[x])
-                            .pillLabels.push(label.label);
-                    }
-                }
-            }
-        });
-        chrome.runtime.sendMessage({ 'predictions': predictions, 'config': conf }, function(response) {
-            console.log(`message from background: ${JSON.stringify(response)}`);
-        });
-    });
-
-    tweets.forEach((tweet) => {
-        var tweetText = getTweetText(tweet);
-        var c = loadedTweets.find((t) => t.content == tweetText);
-
-        if (c.pills.length == 0) return;
-
-        var revealButton = $(
-            "<button type='button' class='btn btn-outline-primary revealTweet'>Reveal Tweet</button>"
-        ).click((e) => revealTweet(e));
-
-        var pillDiv = $("<div class='pillDiv'></div>");
-
-        var parent = tweet.parentElement;
-
-        if (!parent.nextElementSibling) {
-            pillDiv.appendTo(parent.parentElement);
-
-            var t = loadedTweets.find((t) => t.content == tweetText);
-
-            t.pills.forEach((p) => pillDiv.append(p[0]));
-
-            addBlur(parent, revealButton);
-        } else {
-            var t = loadedTweets.find((t) => t.content == tweetText);
-            t.pills.forEach((p) => tweet.parentElement.nextSibling.append(p[0]));
+          if (!t.pillLabels.includes(label.label)) {
+            loadedTweets
+              .find((tweet) => tweet.content == text[x])
+              .pills.push(pill);
+            loadedTweets
+              .find((tweet) => tweet.content == text[x])
+              .pillLabels.push(label.label);
+          }
         }
-
-        tweet.addEventListener("click", function(e) {
-            e.preventDefault();
-            e.stopPropagation();
-        });
+      }
     });
+
+    count += 1;
+
+    if (count % 10 == 0) {
+      chrome.runtime.sendMessage(
+        { predictions: predictions, config: conf },
+        function (response) {
+          console.log(`message from background: ${JSON.stringify(response)}`);
+        }
+      );
+    }
+  });
+
+  tweets.forEach((tweet) => {
+    var tweetText = getTweetText(tweet);
+    var c = loadedTweets.find((t) => t.content == tweetText);
+
+    if (c.pills.length == 0) return;
+
+    var revealButton = $(
+      "<button type='button' class='btn btn-outline-primary revealTweet'>Reveal Tweet</button>"
+    ).click((e) => revealTweet(e));
+
+    var pillDiv = $("<div class='pillDiv'></div>");
+
+    var parent = tweet.parentElement;
+
+    if (!parent.nextElementSibling) {
+      pillDiv.appendTo(parent.parentElement);
+
+      var t = loadedTweets.find((t) => t.content == tweetText);
+
+      t.pills.forEach((p) => pillDiv.append(p[0]));
+
+      addBlur(parent, revealButton);
+    } else {
+      var t = loadedTweets.find((t) => t.content == tweetText);
+      t.pills.forEach((p) => tweet.parentElement.nextSibling.append(p[0]));
+    }
+
+    tweet.addEventListener("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+    });
+  });
 };
 
 getTweetText = (tweet) => {
-    var textChild = tweet.children[1].children[1].children.length == 3 ? 0 : 1;
-    var text = tweet.children[1].children[1].children[textChild].children[0];
-    var tweetText = "";
+  var textChild = tweet.children[1].children[1].children.length == 3 ? 0 : 1;
+  var text = tweet.children[1].children[1].children[textChild].children[0];
+  var tweetText = "";
 
-    if (text !== undefined) {
-        for (let span of text.children) {
-            if (span.firstChild.tagName != "DIV") {
-                tweetText += span.firstChild.textContent;
-            }
-        }
+  if (text !== undefined) {
+    for (let span of text.children) {
+      if (span.firstChild.tagName != "DIV") {
+        tweetText += span.firstChild.textContent;
+      }
     }
-    return tweetText;
+  }
+  return tweetText;
 };
 
 revealTweet = (e) => {
-    e.target.previousElementSibling.classList.add("hidden");
-    e.target.classList.add("hidden");
-    e.target.previousElementSibling.previousElementSibling.classList.remove(
-        "blur"
-    );
+  e.target.previousElementSibling.classList.add("hidden");
+  e.target.classList.add("hidden");
+  e.target.previousElementSibling.previousElementSibling.classList.remove(
+    "blur"
+  );
 };
 
 addBlur = (parent, revealButton) => {
-    parent.classList.add("blur");
-    revealButton.appendTo(parent.parentElement);
+  parent.classList.add("blur");
+  revealButton.appendTo(parent.parentElement);
+};
+
+profiling = () => {
+  var profileHeader = document.querySelector("[aria-label='Profile timelines']")
+    .previousElementSibling;
+
+  var statsDiv = $("<div class='statsDiv'></div>");
+  statsDiv.appendTo(profileHeader);
+
+  var text = $("<h4 class='profileStats'>Profile Summary</h4>");
+  text.appendTo(statsDiv);
+
+  chrome.runtime.sendMessage({ config: conf }, function (response) {
+    JSON.parse(response).data.forEach((r) => {
+      var label = r.label;
+      var percentage = parseInt(r.probability * 100);
+
+      var pill = $(
+        `<span class="badge rounded-pill pill" style='background-color:${
+          colours[label]
+        }'">${label.replace("_", " ")}: ${percentage}%</span>`
+      );
+      pill.appendTo(statsDiv);
+    });
+  });
 };
